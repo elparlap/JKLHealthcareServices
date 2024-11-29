902416f9f6f0e23776d081d37099c874089e525b
elparlap <premsaicharanelparla@gmail.com>
2024-11-29 22:40:43 +0530

    chore: Remove unnecessary files and code

--- Models/Appointment.cs
+++
@@ -1,6 +0,0 @@
-public class Appointment
-{
-    public int Id { get; set; }
-    public int PatientId { get; set; }
-    public DateTime DateTime { get; set; }
-}
--- Models/Patient.cs
+++
@@ -1,7 +0,0 @@
-public class Patient
-{
-    public int Id { get; set; }
-    public required string Name { get; set; } // Using 'required' modifier
-    public required string Address { get; set; } // Using 'required' modifier
-    public required string MedicalRecords { get; set; } // Using 'required' modifier
-}
